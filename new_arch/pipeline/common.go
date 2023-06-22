19bee6fae4ad83208c9cbee677d5064bc32156ca
Alon Muroch <alonmuroch@gmail.com>
2023-06-22 15:17:53 +0300

    pipeline common

--- new_arch/pipeline/common.go
+++ new_arch/pipeline/common.go
@@ -1,6 +1,7 @@
 package pipeline
 
 import (
+	ssz "github.com/ferranbt/fastssz"
 	"github.com/pkg/errors"
 	"ssv-experiments/new_arch/p2p"
 	"ssv-experiments/new_arch/qbft"
@@ -30,4 +31,36 @@ func DecodeMessage(pipeline *Pipeline, objects ...interface{}) (error, []interfa
 	default:
 		return errors.New("unsupported message type"), nil
 	}
+}
+
+// Broadcast will broadcast a message to the SSV network
+func Broadcast(t p2p.MsgType) func(pipeline *Pipeline, objects ...interface{}) (error, []interface{}) {
+	return func(pipeline *Pipeline, objects ...interface{}) (error, []interface{}) {
+		data := objects[0].(ssz.Marshaler)
+
+		byts, err := data.MarshalSSZ()
+		if err != nil {
+			return err, nil
+		}
+
+		msg := &p2p.Message{
+			MsgType: t,
+			MsgID:   pipeline.Runner.Identifier,
+			Data:    byts,
+		}
+
+		// broadcast
+
+		return nil, []interface{}{msg}
+	}
+}
+
+// BroadcastBeacon broadcasts to the beacon chain
+func BroadcastBeacon(pipeline *Pipeline, objects ...interface{}) (error, []interface{}) {
+	for _, item := range objects {
+		if _, encodable := item.(ssz.Marshaler); encodable {
+			// broadcast
+		}
+	}
+	return nil, nil
 }
